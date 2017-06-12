926ade9ec5b8e86c5c06b938f726908d6a5ca5e4
Tongle <Tongle@sansi.com>
2017-06-12 09:54:32 +0800

    first commit

---
+++ TongPopInstall/TongButton.swift
@@ -0,0 +1,21 @@
+//
+//  TongButton.swift
+//  TongPopInstall
+//
+//  Created by tongle on 2017/6/9.
+//  Copyright © 2017年 tong. All rights reserved.
+//
+
+import UIKit
+
+class TongButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
