7409d5aae14345afc2f0540fd244bafd5ca705cc
Tongle <Tongle@sansi.com>
2017-07-10 09:16:10 +0800

    add file

--- TongPopInstall/TongButton.swift
+++ TongPopInstall/TongButton.swift
@@ -10,12 +10,80 @@ import UIKit
 
 class TongButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        //缩放动画
+        var scale = self.pop_animation(forKey: "scale")as?POPSpringAnimation
+        if scale != nil {
+            scale?.toValue = NSValue(cgPoint:CGPoint(x: 0.8, y: 0.8))
+        }else{
+       scale = POPSpringAnimation(propertyNamed: kPOPViewScaleXY)
+            scale?.toValue = NSValue(cgPoint:CGPoint(x: 0.8, y: 0.8))
+            scale?.springBounciness = 20
+            scale?.springSpeed = 5
+            self.pop_add(scale, forKey: "scale")
+            
+        }
+        //旋转动画
+        var rotate = self.layer.pop_animation(forKey: "rotate")as? POPSpringAnimation
+        if rotate != nil {
+            rotate?.toValue = Double.pi / 6
+        }else{
+            rotate = POPSpringAnimation(propertyNamed: kPOPLayerRotation)
+            rotate?.toValue = Double.pi / 6
+            rotate?.springBounciness = 20
+            rotate?.springSpeed = 18
+            self.layer.pop_add(rotate, forKey: "rotate")
+        }
+        
+        
     }
-    */
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        //缩放动画
+        var scale = self.pop_animation(forKey: "scale")as?POPSpringAnimation
+        if scale != nil {
+            scale?.toValue = NSValue(cgPoint:CGPoint(x: 1, y: 1))
+        }else{
+            scale = POPSpringAnimation(propertyNamed: kPOPViewScaleXY)
+            scale?.toValue = NSValue(cgPoint:CGPoint(x: 1, y: 1))
+            scale?.springBounciness = 20
+            scale?.springSpeed = 5
+            self.pop_add(scale, forKey: "scale")
+            
+        }
+        //旋转动画
+        var rotate = self.layer.pop_animation(forKey: "rotate")as? POPSpringAnimation
+        if rotate != nil {
+            rotate?.toValue = 0
+        }else{
+            rotate = POPSpringAnimation(propertyNamed: kPOPLayerRotation)
+            rotate?.toValue = 0
+            rotate?.springBounciness = 20
+            rotate?.springSpeed = 18
+            self.layer.pop_add(rotate, forKey: "rotate")
+        }
 
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TongPopInstall/ViewController.swift
+++ TongPopInstall/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func didTapHamburgerButton(sender:UIButton){
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
